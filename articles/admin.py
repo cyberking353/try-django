ad679649bed3f4f98182ec44ddfa37fa91013fcb
salim bello <salimbello32@gmail.com>
2022-09-20 23:31:57 +0100

    admin.py modified

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Article
 # Register your models here.
-
+# added this comment for testing
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id','title',]
     search_fields = ['id','title',]
