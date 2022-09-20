2578d82b3bcd5497940c5cc3fac514f2ba270ecc
salim bello <salimbello32@gmail.com>
2022-09-20 23:37:28 +0100

    settings updated

--- my_demo/settings.py
+++ my_demo/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-_-ziv+gbj2+^oqe6dg@7504tp923++p!x&0j*-i)xw8_a*8bcq')
-print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
